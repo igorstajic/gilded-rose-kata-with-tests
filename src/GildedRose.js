4d32e14e829455461f318cd530cd43c3023fd335
Igor <igorstajic273@gmail.com>
2019-06-29 19:26:49 +0200

    Refactoring - Changes to updateQuality method. - Added some helper funcitons.

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -4,81 +4,85 @@
     
     Leeroy <lerooy@example.com>
 */
-var GildedRose = function () {
+
+function isLegendary(item) {
+  const legendaryItemNames = ['Sulfuras, Hand of Ragnaros'];
+  return legendaryItemNames.includes(item.name);
+}
+
+function isAgingWell(item) {
+  return item.name.startsWith('Aged');
+}
+
+function isBackstagePass(item) {
+  return item.name.startsWith('Backstage passes');
+}
+
+function normalizeQualityValue(quality) {
+  // The Quality of an item is never more than 50.
+  // The Quality of an item is never negative.
+  if (quality < 0) {
+    return 0;
+  }
+  if (quality > 50) {
+    return 50;
+  }
+  return quality;
+}
+
+function getUpdatedItemQuality(item) {
+  if (isLegendary(item)) {
+    // Legendary item quality NEVER alters.
+    return item.quality;
+  }
+
+  if (isAgingWell(item) || isBackstagePass(item)) {
+    // Aged item increases in Quality the older it gets.
+    let qualityChangeModifier = 1;
+    if (item.sellIn <= 10) {
+      qualityChangeModifier = 2;
+    }
+    if (item.sellIn <= 5) {
+      qualityChangeModifier = 3;
+    }
+    if (item.sellIn <= 0) {
+      // Quality drops to 0 after the sellIn date.
+      return 0;
+    }
+    return normalizeQualityValue(item.quality + qualityChangeModifier);
+  }
+
+  // For other items, once the sell by date has passed, Quality degrades twice as fast.
+  let qualityChangeModifier = item.sellIn <= 0 ? 2 : 1;
+
+  return normalizeQualityValue(item.quality - qualityChangeModifier);
+}
+
+function getUpdatedSellInValue(item) {
+  if (isLegendary(item)) {
+    return item.sellIn;
+  }
+  // Once the sell by date has passed, sellIn decreases twice as fast.
+  const changeModifier = item.sellIn < 0 ? 2 : 1;
+
+  return item.sellIn - changeModifier;
+}
+
+var GildedRose = function() {
   var items = [];
-  items.push(new Item("+5 Dexterity Vest", 10, 20));
-  items.push(new Item("Aged Brie", 2, 0));
-  items.push(new Item("Elixir of the Mongoose", 5, 7));
-  items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-  items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-  items.push(new Item("Conjured Mana Cake", 3, 6));
+  items.push(new Item('+5 Dexterity Vest', 10, 20));
+  items.push(new Item('Aged Brie', 2, 0));
+  items.push(new Item('Elixir of the Mongoose', 5, 7));
+  items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
+  items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+  items.push(new Item('Conjured Mana Cake', 3, 6));
   GildedRose.updateQuality(items);
 };
 
-GildedRose.updateQuality = function (items) {
-  for (var i = 0; i < items.length; i++) {
-    if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-      //TODO: Improve this code.
-      if (items[i].quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 6) {
-              items[i].quality = items[i].quality + 1
-            }
-        }
-        //Increases the Quality of the stinky cheese if its 11 days to due date.
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 11) {
-              items[i].quality = items[i].quality + 1
-            }
-        }
-        if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
-          if (items[i].sellIn < 11) {
-            // See revision number 2394 on SVN.
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-      items[i].sellIn = items[i].sellIn - 1;
-    }
-    if (items[i].sellIn < 0) {
-      if ("Aged Brie" != items[i].name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-          if (items[i].quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          //TODO: Fix this.
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-        if ("Aged Brie" == items[i].name && items[i].sellIn <= 0)
-            items[i].quality = 0;
-      } // of for.
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name)
-      if (items[i].quality > 50) items[i].quality = 50;
-  }
-  return items;
+GildedRose.updateQuality = function(items) {
+  return items.map(item => ({
+    name: item.name,
+    quality: getUpdatedItemQuality(item),
+    sellIn: getUpdatedSellInValue(item),
+  }));
 };
